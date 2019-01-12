40c80c911ff107c525d79079158fc9a36ff7b248
DimaGashko <dgashko@mail.ua>
2019-01-12 07:41:14 +0200

    initController

--- src/com/Index.java
+++ src/com/Index.java
@@ -2,14 +2,19 @@ package com;
 
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 
-public class Index {
-    @FXML private JFXTextField fxNoAlign__field1;
-    @FXML private JFXTextField fxNoAlign__field2;
-    @FXML private JFXTextField fxNoAlign__field3;
-    @FXML private JFXTextField fxNoAlign__field4;
-    @FXML private JFXTextField fxNoAlign__field5;
-    @FXML private JFXTextField fxNoAlign__field6;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Index implements Initializable {
+    @FXML private JFXTextField fxNoError__field1;
+    @FXML private JFXTextField fxNoError__field2;
+    @FXML private JFXTextField fxNoError__field3;
+    @FXML private JFXTextField fxNoError__field4;
+    @FXML private JFXTextField fxNoError__field5;
+    @FXML private JFXTextField fxNoError__field6;
 
     @FXML private JFXTextField fxError__field1;
     @FXML private JFXTextField fxError__field2;
@@ -24,4 +29,34 @@ public class Index {
     @FXML private JFXTextField fxFixed__field4;
     @FXML private JFXTextField fxFixed__field5;
     @FXML private JFXTextField fxFixed__field6;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //initValidators();
+    }
+
+    private void initValidators(JFXTextField node) {
+        node.focusedProperty().addListener((o, oldVal, newVal) -> {
+            if (!newVal) node.validate();
+        });
+
+        node.textProperty().addListener((o, oldVal, newVal) -> {
+            node.validate();
+        });
+    }
+
+    private void initFixedValidators(JFXTextField node) {
+        node.focusedProperty().addListener((o, oldVal, newVal) -> {
+            node.setAlignment(Pos.TOP_LEFT);
+            if (!newVal) node.validate();
+            node.setAlignment(Pos.CENTER);
+        });
+
+        node.textProperty().addListener((o, oldVal, newVal) -> {
+            node.setAlignment(Pos.TOP_LEFT);
+            node.validate();
+            node.setAlignment(Pos.CENTER);
+        });
+    }
+
 }
