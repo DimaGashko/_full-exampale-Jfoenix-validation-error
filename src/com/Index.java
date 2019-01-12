81830b24e61e57d0f1a16207bfab8d8234ed92c0
DimaGashko <dgashko@mail.ua>
2019-01-12 07:14:51 +0200

    baseStyles

--- src/com/Index.java
+++ src/com/Index.java
@@ -1,4 +1,24 @@
 package com;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
 public class Index {
+    @FXML private JFXTextField fxNoAlign__field1;
+    @FXML private JFXTextField fxNoAlign__field2;
+    @FXML private JFXTextField fxNoAlign__field3;
+    @FXML private JFXTextField fxNoAlign__field4;
+    @FXML private JFXTextField fxNoAlign__field5;
+
+    @FXML private JFXTextField fxError__field1;
+    @FXML private JFXTextField fxError__field2;
+    @FXML private JFXTextField fxError__field3;
+    @FXML private JFXTextField fxError__field4;
+    @FXML private JFXTextField fxError__field5;
+
+    @FXML private JFXTextField fxFixed__field1;
+    @FXML private JFXTextField fxFixed__field2;
+    @FXML private JFXTextField fxFixed__field3;
+    @FXML private JFXTextField fxFixed__field4;
+    @FXML private JFXTextField fxFixed__field5;
 }
